c162ff4222eda5f6fed60ecbc656e4c10c2087d8
luchomolina92 <luchomolina53@gmail.com>
2021-08-08 23:45:43 -0300

    cambio

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ let port = 3030;
 
 app.use(express.static('public'));
 
-/* ROUTERS
-headerRouter = require('.routes/headerRouter');
+
 /*ROUTERS*/
 /*headerRouter = require('.routes/headerRouter');
 loginRouter = require('.routes/loginRouter');
 registerRouter = require('.routes/registerRouter'); */
-
-
-
-/*ROUTES
-app.use('/register',registerRouter);
 
 /*ROUTES*/
 /*app.use('/register',registerRouter);
@@ -24,7 +18,7 @@ app.use('/login', loginRouter);
 app.use('/header', headerRouter); */
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/lucho.html'));
+    res.sendFile(path.join(__dirname,'/views/home.html'));
 })
 
 app.get('/header', (req, res) => {
@@ -64,14 +58,12 @@ app.get('/climatizacion', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/climatizacion.html'));
 })
 
-/*app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/home.html'));
-})*/
+
 
 app.get('/tvysonido', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/tvysonido.html'));
 })
-/* app.get('/productDetail', (req, res) => {*/
+
  app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/productDetail.html'));
 })
