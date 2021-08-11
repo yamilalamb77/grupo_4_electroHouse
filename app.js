461e088520982172e0b9483ece043f19967503a4
yamilalamb77 <yamila.lamb.77@gmail.com>
2021-08-11 00:35:37 -0300

    login y register

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get('/tvYSonido', (req, res) => {
  app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/productDetail.html'));
 })
-/* app.get('*', (req, res) => {
+ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/404.html'))
-})  */
+})  
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}`)); 
