76ebf7bb2285465bca34aad42ff067b46dd6afaa
luchomolina92 <luchomolina53@gmail.com>
2021-09-05 18:45:15 -0300

    ruta

--- src/controllers/admincontroller.js
+++ src/controllers/admincontroller.js
@@ -58,11 +58,11 @@ module.exports = {
 
         writeProductsJSON(products)
 
-        res.redirect('/admin/products')
+        res.redirect('admin/products')
     }, 
     productEdit: (req, res) => {
         let product = products.find(product => product.id === +req.params.id)
-        res.render('/admin/adminProductEditForm', {
+        res.render('admin/adminProductEditForm', {
             categories, 
             subcategories,
             product
@@ -95,7 +95,7 @@ module.exports = {
         writeProductsJSON(products)
 
 
-        res.redirect('admin/products')
+        res.redirect('/admin/products')
     },
     productDestroy: (req, res) => {
         products.forEach( product => {
