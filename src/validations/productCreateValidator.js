a6590712dcc928bea2d39666fd02c89f025b66fc
yamilalamb77 <yamila.lamb.77@gmail.com>
2021-11-03 00:19:06 -0300

    input de color

--- src/validations/productCreateValidator.js
+++ src/validations/productCreateValidator.js
@@ -1,23 +1,61 @@
-let { check } = require('express-validator');
+let { check, body } = require('express-validator');
 
 module.exports = [
-    check('name')
-    .notEmpty()
-    .withMessage("El campo nombre no puede ir vacío")
-    .isLength({ min: 3 })
-    .withMessage("Ingrese más de 3 caracteres"),
+    body('name')
+    .custom((value)=>{
+        regexName = /^[a-zA-Z]{2,}*$/
+        return regexName.test(value)
+    })
+    .withMessage('Introduzca un nombre valido'),
 
     check("category")
     .notEmpty()
-    .withMessage("Debes elegir una categoría"),
+    .withMessage("Debes elegir una categoría")
+    .isNumeric()
+    .isInt({
+        min: 1,
+        max: 4
+    })
+    .withMessage("La categoria seleccionada no existe"),
 
     check('subcategory')
     .notEmpty()
-    .withMessage('Debes elegir una subcategoría'),
+    .withMessage('Debes elegir una subcategoría')
+    .isNumeric()
+    .isInt({
+        min: 1,
+        max: 7
+    })
+    .withMessage("La subcategoria seleccionada no existe"),
+   
 
     check('price')
     .notEmpty()
     .withMessage('Coloca un precio')
     .isNumeric()
+    .withMessage("Solo puedes ingresar números"),
+
+    check('discount')
+    .isNumeric()
     .withMessage("Solo puedes ingresar números")
+    .isInt({
+        min: 0,
+        max: 100
+    })
+    .withMessage("Introduzca un valor entero entre 0 y 100"),
+
+
+    body('description')
+    .notEmpty()
+    .withMessage('debes escribir unas descripcion').bail()
+    .custom((value)=>{
+        regexDescription = /^[\w\s]*$/
+        return regexDescription.test(value)
+
+    })
+    .withMessage('Introduzca una descripcion valida'),
+
+
+
+
 ]
