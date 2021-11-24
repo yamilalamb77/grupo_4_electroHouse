de364efa7f6267bd8f787e6bad3c40c87daced2c
luchomolina92 <luchomolina53@gmail.com>
2021-11-24 16:11:08 -0300

    search

---
+++ public/js/profileVal.js
@@ -0,0 +1,22 @@
+function qs (element) {
+    return document.querySelector(element)
+}
+
+
+window.addEventListener("load", function(){
+    let $buttonDel = qs("#evil-btn-submit")
+    let $formDel = qs("#formDel")
+    
+    $buttonDel.addEventListener("click", function(){
+        let opcion = confirm("¿Estas seguro de querer eliminar tu cuenta? está accion no se puede revertir")
+        
+        $formDel.addEventListener("submit", function(event){
+            event.preventDefault()
+
+            if(opcion === true){
+                $formDel.submit()
+            } 
+        })
+    })
+
+})
