e848b0ee0317c9c70a9dccefafba2a86f78c0eed
luchomolina92 <luchomolina53@gmail.com>
2021-12-02 09:38:08 -0300

    order controller

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,8 +1,16 @@
 let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/api/apiController.js')
+let orderController = require('../controllers/api/orderController')
 
 router.get('/categories', controller.allCategories);
 router.get('/categories/:id', controller.oneCategory);
+
+/* Cart */
+router.post('/cart/:product/:quantity/:user', orderController.addToCart)
+router.get('/cart/:user', orderController.productsInCart)
+router.delete('/cart/removeOne/:item/:user', orderController.removeOneFromCart)
+router.delete('/cart/removeAll/:item/:user', orderController.removeAllFromCart)
+router.delete('/cart/clearCart/:user', orderController.clearCart)
 
 module.exports = router;
